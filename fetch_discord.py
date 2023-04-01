f8455c797c7c437587a7c3534594d1efa6edb417
rishabhpoddar <rishabh.poddar@gmail.com>
2023-04-01 20:10:00 +0530

    creates discord_threads.json in processed

--- fetch_discord.py
+++ fetch_discord.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 url = "https://community.supertokens.com/api/threads"
 params = {
@@ -9,25 +10,33 @@ headers = {
     "Content-Type": "application/json"
 }
 
+# read existing threads from processed/discord_threads.json if it exists
+try:
+    with open("processed/discord_threads.json", "r") as f:
+        threads = json.loads(f.read())
+except:
+    threads = []
+
 prev_cursor = None
 while True:
     if prev_cursor is not None:
         params["cursor"] = prev_cursor
+    print("fetching threads with cursor: " + str(prev_cursor))
     response = requests.get(url, headers=headers, params=params)
     data = response.json()
-    threads = data["threads"]
-    for thread in threads:
+    found_old_threads = False
+    for thread in data["threads"]:
         if len(thread["messages"]) <= 1:
             # this is cause previously, we used to not create threads for messages.
             continue
-        # Do something with each thread
-        for message in thread["messages"]:
-            if (message["author"] is None or message["body"] is None):
-                continue
-            # Do something with each message
-            print(message["author"]["username"] + ": " + message["body"])
-            print()
-        print("================= " + str(prev_cursor) + " =================")
-    if data["nextCursor"] is None or data["nextCursor"]["prev"] is None:
+        if thread["id"] in [t["id"] for t in threads]:
+            found_old_threads = True
+            continue
+        threads.append(thread)
+    if data["nextCursor"] is None or data["nextCursor"]["prev"] is None or found_old_threads:
         break
     prev_cursor = data["nextCursor"]["prev"]
+
+# save threads as a json file
+with open("processed/discord_threads.json", "w") as f:
+    f.write(json.dumps(threads, indent=4))
